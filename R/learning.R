61e110e3416b6c8ded6ffd61b7970eaeff8ae8f6
Ibai Lenitz Etxaburu <lenitz@chalmers.se>
2023-01-17 13:40:51 +0100

    showing off mutate, filter, arrange

--- R/learning.R
+++ R/learning.R
@@ -94,21 +94,52 @@ nhanes_small %>%
 # exercise 7.8 ------------------------------------------------------------
 
 nhanes_small %>%
-    select(bp_sys_ave,education)
+  select(bp_sys_ave, education)
 
 nhanes_small %>%
-    rename(bp_sys = bp_sys_ave,
-           bp_dia = bp_dia_ave)
+  rename(
+    bp_sys = bp_sys_ave,
+    bp_dia = bp_dia_ave
+  )
 
-#Re-write this piece of code using the “pipe” operator:
-    #select(nhanes_small, bmi, contains("age"))
+# Re-write this piece of code using the “pipe” operator:
+# select(nhanes_small, bmi, contains("age"))
 
-nhanes_small%>%
-    select(bmi, contains("age"))
+nhanes_small %>%
+  select(bmi, contains("age"))
 
-#blood_pressure <- select(nhanes_small, starts_with("bp_"))
-#rename(blood_pressure, bp_systolic = bp_sys_ave)
+# blood_pressure <- select(nhanes_small, starts_with("bp_"))
+# rename(blood_pressure, bp_systolic = bp_sys_ave)
 
-nhanes_small%>%
-    select(starts_with("bp")) %>%
-    rename(bp_systolic = bp_sys_ave)
+nhanes_small %>%
+  select(starts_with("bp")) %>%
+  rename(bp_systolic = bp_sys_ave)
+
+# Filtering rows ----------------------------------------------------------
+
+nhanes_small %>%
+  filter(phys_active != "No")
+nhanes_small %>%
+  filter(bmi >= 25 & phys_active == "No")
+nhanes_small %>%
+  filter(bmi == 25 | phys_active == "No")
+
+
+# Arranging rows ----------------------------------------------------------
+
+nhanes_small %>%
+  arrange(desc(age), bmi, education)
+
+
+# Mutating columns --------------------------------------------------------
+
+
+
+nhanes_update <- nhanes_small %>%
+  mutate(
+    age_month = age * 12,
+    logged_bmi = log(bmi),
+    age_weeks = age_month * 4,
+    old = if_else(age >= 30, "old", "young")
+  )
+
